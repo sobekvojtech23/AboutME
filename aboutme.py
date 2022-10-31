349db4c94ddea5c7f43450e50f38089f40575dbf
sobekvojtech23 <vojt.sobek@seznam.cz>
2022-10-31 19:38:24 +0100

    Something simple...

---
+++ aboutme.py
@@ -0,0 +1,17 @@
+#written in python
+
+class HighSchool_Student:
+
+    def __init__(self):
+        self.name = "Shadowman_23"
+        self.origin = "Czech republic"
+        self.role = "Computer Science Student"
+        self.language_spoken = ["czech", "english", "german"]
+        self.programming_languages = ["C# and  C","Python"]
+        self.interest = ["programming", "data science", "linux administration"]
+        self.certification = ["CCNA"]
+
+
+me = HighSchool_Student()
+
+print("I hope you will enjoy my GitHub page :D.")
